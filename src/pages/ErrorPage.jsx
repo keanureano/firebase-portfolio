c9db684cd43dc5be2eb25704e322a8e51cb65364
Keanu Reaño <reanokeanu@gmail.com>
2023-04-02 00:12:07 +0800

    add white noise on errorpage

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -5,7 +5,8 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div className="flex flex-col h-screen justify-center bg-black-900 text-white/90 items-center">
+    <div className="flex flex-col items-center justify-center h-screen bg-black-900 text-white/90">
+      <div className="bg"></div>
       <h1>Oops!</h1>
       <p>An unexpected error has occured.</p>
       <p>
