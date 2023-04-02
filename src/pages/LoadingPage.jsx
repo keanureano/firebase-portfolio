7122662308be910adcd7dadd723fb7a814fa63e7
Keanu Reaño <reanokeanu@gmail.com>
2023-04-02 14:24:13 +0800

    add logo and favicon

--- src/pages/LoadingPage.jsx
+++ src/pages/LoadingPage.jsx
@@ -1,9 +1,12 @@
+import logo from "../assets/logo.png";
+
 export default function LoadingPage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-black-900 text-white/90">
       <div className="bg"></div>
-      <div className="fade-in-onload">
-        <h1>Keanu Reaño</h1>
+      <div className="flex flex-col items-center fade-in-onload">
+        <img src={logo} className="w-32" alt="logo"/>
+        <h1 className="title">Keanu Reaño</h1>
       </div>
     </div>
   );
