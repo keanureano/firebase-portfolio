86e4309c2ac682d2fbdfdcebdbc9038dfb18f110
Keanu Reaño <reanokeanu@gmail.com>
2023-07-28 23:12:38 +0800

    add homepage page 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
 export default function Home() {
-  return <main>Hello world</main>;
+  return (
+    <main>
+      <div className="md:flex">
+        <div className="w-full space-y-4">
+          <p>Solving problems with Software.</p>
+          <p className="px-2 py-1 border-l border-black">
+            I love the process of transforming abstract problems into tangible
+            and impactful solutions. From simple but captivating front-end
+            interfaces to robust back-end functionalities, I always ensure
+            accuracy and user satisfaction.
+          </p>
+          <p>
+            Constantly learning and improving, I strive to refine my skills in
+            Software Development.
+          </p>
+        </div>
+        <div className="w-full space-y-4">
+          <p>Simplify Complexity.</p>
+          <p>Avoid Redundancy.</p>
+          <p>Always Be Learning.</p>
+          <p>Enjoy What You Do.</p>
+        </div>
+      </div>
+    </main>
+  );
 }
